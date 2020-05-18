449875f35c48a706813255b746f692d48e6a0674
pieter1998@protonmail.com <ba8b9d3612615214286ffddbb58bafcc>
2020-05-18 18:46:01 +0200

    simple request timer using curl and zsh

---
+++ timeRequests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+vared -p 'enter a address to connect to: ' -c address
+vared -p 'amount of pings: ' -c ping
+
+total=0
+
+for ((i = 0 ; i < $ping; i++)); do
+	time=$(curl -w "%{time_total}" -o /dev/null -s $address)
+	echo "request time:  ${time}"
+	((total=total+time)) 
+done
+
+echo "total time: ${total}"
